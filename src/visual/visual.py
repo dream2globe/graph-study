0a4e42ca6ec12d6b0e311451299a62b4f948da37
Kang Seonghyeon <Kang Seonghyeon>
2023-02-16 02:24:51 +0900

    first commit

--- src/visual/visual.py
+++ src/visual/visual.py
@@ -5,9 +5,9 @@ from PIL import Image
 from playwright.sync_api import sync_playwright
 
 
-def html2ani(source, target, temp=None):
+def html2ani(source, target, temp: None | str = None):
     if temp is None:
-        temp = Path().cwd() / "data" / "visual" / "temp"
+        temp = Path().cwd() / "temp"
     html_files = source.glob("*.html")
     for html in html_files:
         with sync_playwright() as p:
@@ -18,8 +18,10 @@ def html2ani(source, target, temp=None):
                 timeout=60000,
                 wait_until="networkidle",
             )
-            page.screenshot(path=temp / f"{html.stem}.png", full_page=True)
+            temp_file = temp / f"{html.stem}.png"
+            page.screenshot(path=temp_file, full_page=True)
             browser.close()
+    # target
     png_files = [str(path) for path in target.glob("*.png")]
     pngs = [Image.open(png) for png in sorted(png_files)]
     imageio.mimsave("test.gif", pngs, fps=2)
@@ -28,3 +30,4 @@ def html2ani(source, target, temp=None):
 if __name__ == "__main__":
     html_path = Path.cwd() / "data" / "visual" / "html"
     ani_path = Path.cwd() / "data" / "visual" / "ani"
+    html2ani(html_path, ani_path)
