c93faa7022fefb9fa5349d1a44aef9622874e6c8
Kang Seonghyeon <shyeon.kang@gmail.com>
2023-11-28 23:38:33 +0900

    updated main.py

--- src/main.py
+++ src/main.py
@@ -1,35 +1,43 @@
 import pickle
+import warnings
+from collections import defaultdict
+from itertools import product
 from pathlib import Path
+from typing import Literal
 
 import lightgbm as lgb
 import numpy as np
 import pandas as pd
+from sklearn.covariance import GraphicalLassoCV
+from sklearn.exceptions import ConvergenceWarning
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
 
-from models.rank import radiorank
+from models.rank import pagerank, radiorank
 from src.utils.graph import build_nx_graph
-from src.utils.logger import get_logger
 
 # General
-logger = get_logger()
+weight = "value"
 random_seed = 42
+warnings.filterwarnings("ignore", category=ConvergenceWarning)
 
 # For pagerank
-alpha = 0.1  # damping factor
-weight = "value"  # weight key in graph
-pos = pd.read_csv("data/processed/position.csv")  # node positions in graph
+use_pos = False
+if use_pos == True:
+    pos = pd.read_csv("data/processed/position.csv")  # node positions in graph
+else:
+    pos = None
 
 # Cache of prediction
-## structure : {"y": {"[x]" : mse, "[x1, x2]" : mse}}
+## structure : {"y_num_xs" : [("features": set[str], "mse": float)]}
 path_cache_mse = Path("data/evaluation/cache_mse.pickle")
 if path_cache_mse.exists():
     with open(path_cache_mse, "rb") as f:
         cache_mse = pickle.load(f)
 else:
-    cache_mse = dict()
+    cache_mse = defaultdict(list)
 
 
 def eval_mse_one(
@@ -37,14 +45,24 @@ def eval_mse_one(
     testset: pd.DataFrame,
     xs: list[str],
     y: str,
-    random_seed: int = 42,
-) -> float:
-    x_train, y_train = trainset[xs].values, trainset[y].values
-    x_test, y_test = testset[xs].values, testset[y].values
-    model = lgb.LGBMRegressor(random_state=random_seed)
+    model,
+):
+    global cache_mse
+    key_name = model.__class__.__name__ + "_" + str(y) + "_" + str(len(xs))
+    # Search for MSE in cache using the feature names
+    recodes = cache_mse.get(key_name)
+    if recodes is not None:
+        for features, mse in recodes:
+            if features == set(xs):
+                return mse
+    # If MSE doesn't exist in the cache, prediction logic run
+    x_train, y_train = trainset[xs], trainset[y]
+    x_test, y_test = testset[xs], testset[y]
     model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
-    return mean_squared_error(y_test, y_pred)
+    mse = mean_squared_error(y_test, y_pred)
+    cache_mse[key_name].append((set(xs), mse))  # save to cache
+    return mse
 
 
 def eval_mse_all(
@@ -53,59 +71,111 @@ def eval_mse_all(
     features: list[str],
     start_num: int,
     step: int,
+    model,
 ) -> pd.DataFrame:
-    eval_records = []
-    for num_xs in range(start_num, len(trainset.columns), step):
+    mse_records = []
+    p_bar = tqdm(range(start_num, len(trainset.columns), step), leave=False)
+    for num_xs in p_bar:
+        p_bar.set_description(f"The num of features: {num_xs}")
         xs = features[:num_xs]
         ys = features[num_xs:]
-        logger.info(f"Input variables: {xs}")
-        scores = [eval_mse_one(trainset, testset, xs, y) for y in ys]
-        record = (num_xs, min(scores), np.mean(scores), max(scores))
-        # results
-        eval_records.append(record)
-    eval_mat_df = pd.DataFrame().from_records(
-        eval_records, columns=["num_xs", "min", "mean", "max"]
+        mses = [
+            (num_xs, y, eval_mse_one(trainset, testset, xs, y, model))
+            for y in tqdm(ys, leave=False)
+        ]
+        mse_records.extend(mses)
+    mses_df = pd.DataFrame().from_records(mse_records, columns=["num_xs", "y", "mse"])
+    return mses_df
+
+
+def prepare_dataset() -> tuple[pd.DataFrame, pd.DataFrame]:
+    dataset = pd.read_pickle("data/processed/values.pickle")
+    train, test = train_test_split(
+        dataset, test_size=0.5, random_state=random_seed, shuffle=False
     )
-    return eval_mat_df
+    scaler = StandardScaler().fit(train)
+    train = pd.DataFrame(scaler.transform(train))
+    test = pd.DataFrame(scaler.transform(test))
+    return train, test
+    # test_items = df.columns.tolist()
+    # item_2_idx = {v: k for k, v in enumerate(test_items)}
+    # idx_2_item = {k: v for k, v in enumerate(test_items)}
+
+
+def build_graph(train_df: pd.DataFrame, mat: Literal["corr", "lasso"]):
+    match mat:
+        case "corr":
+            return train_df.corr()
+        case "lasso":
+            return pd.DataFrame(GraphicalLassoCV().fit(train_df).precision_)
+
+
+def get_params_combination(train_df: pd.DataFrame):
+    hyper_params = {
+        "relation": [
+            {"name": "corr", "value": build_graph(train_df, "corr")},
+            {"name": "lasso", "value": build_graph(train_df, "lasso")},
+        ],
+        "rankers": [
+            {"name": "pagerank", "value": pagerank},
+            {"name": "radiorank", "value": radiorank},
+        ],
+        "alphas": np.arange(0.1, 1.0, 0.05).round(2),
+        "models": [
+            # {
+            #     "name": "rf",
+            #     "value": RandomForestRegressor(random_state=random_seed, n_jobs=-1),
+            # },
+            {
+                "name": "lgb",
+                "value": lgb.LGBMRegressor(
+                    random_state=random_seed,
+                    n_jobs=-1,
+                    force_row_wise=True,
+                    verbose=0,
+                ),
+            },
+        ],
+    }
+    return product(*[value for value in hyper_params.values()])
 
 
 if __name__ == "__main__":
-    # Load preprocessed data
-    logger.info(f"Load preprocessed data")
-    value_df = pd.read_pickle("data/processed/values.pickle")
-    test_items = value_df.columns.tolist()
-    item_2_idx = {v: k for k, v in enumerate(test_items)}
-    idx_2_item = {k: v for k, v in enumerate(test_items)}
+    # Preprocessing
+    train_df, test_df = prepare_dataset()
+    combination = get_params_combination(train_df)
 
-    # Prepare training and test data
-    logger.info(f"Prepare training and test data")
-    train, test = train_test_split(
-        value_df, test_size=0.5, random_state=random_seed, shuffle=False
-    )
-    scaler = StandardScaler().fit(train)
-    scaled_train = pd.DataFrame(scaler.transform(train))
-    scaled_test = pd.DataFrame(scaler.transform(test))
-    titles = train.columns.tolist()
-    corr_matrix = scaled_train.corr()
-
-    # Hyperparameter
-    alphas = np.arange(0.1, 1.1, 1)
-
-    logger.info(f"Evaluating prediction performance")
+    # Run main loop
     eval_df_all = pd.DataFrame()
-    for alpha in tqdm(alphas):
+    p_bar = tqdm(list(combination))
+    for relation, ranker, alpha, model in p_bar:
+        p_bar.set_description(
+            f"{relation['name']}, {ranker['name']}, {alpha}, {model['name']}"
+        )
         # Build a graph using a correlation matrix
-        G = build_nx_graph(corr_matrix, titles, pos=pos, threshold=0)
+        G = build_nx_graph(
+            relation["value"], list(train_df.columns), pos=pos, threshold=0
+        )
 
         # Features which are ordered by importance
-        selected_nodes = radiorank(G, alpha, weight)
+        selected_nodes = ranker["value"](G, alpha, max_iter=500)
 
         # Evaluating prediction performance
         eval_df_one = eval_mse_all(
-            scaled_train, scaled_test, selected_nodes, start_num=1, step=10
+            train_df,
+            test_df,
+            selected_nodes,
+            start_num=1,
+            step=1,
+            model=model["value"],
         )
-        eval_df_one["alpha"] = alpha
-        eval_df_all = pd.concat([eval_df_all, eval_df_one])
 
-    with open("data/evaluation/mse_test.pickle", "wb") as fw:
-        pickle.dump(eval_df_all, fw)
+        # Record MSEs to a file
+        eval_df_one["relation"] = relation["name"]
+        eval_df_one["ranker"] = ranker["name"]
+        eval_df_one["alpha"] = alpha
+        eval_df_one["model"] = model["name"]
+        eval_df_all = pd.concat([eval_df_all, eval_df_one])
+        eval_df_all.to_pickle("data/evaluation/mse.pickle")  # Overwrite on every loop
+        with open("data/evaluation/cache_mse.pickle", "wb") as fw:
+            pickle.dump(cache_mse, fw)
